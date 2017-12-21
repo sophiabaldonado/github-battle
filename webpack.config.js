d4bb2a297cda8e3b44c60ffa087f1427ee22884c
sophia <sophiabaldonado@gmail.com>
2017-12-21 11:52:10 -0800

    use sass;

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	module: {
 		rules: [
 			{ test: /\.(js)$/, use: 'babel-loader' },
-			{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+			{ test: /\.scss$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ]}
 		]
 	},
 	output: {
