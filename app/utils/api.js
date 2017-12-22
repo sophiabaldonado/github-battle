03bd710eec7a8a2b07bd702a2b93712aca95664a
sophia <sophiabaldonado@gmail.com>
2017-12-21 19:51:27 -0800

    use axios http library;

---
+++ app/utils/api.js
@@ -0,0 +1,8 @@
+const axios = require('axios')
+
+module.exports = {
+	fetchPopularRepos: (language) => {
+		let encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
+		return axios.get(encodedURI).then(res => res.data.items)
+	}
+}
